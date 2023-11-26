b2c199829255ed7c3908c44dfdee18fcf5fec4f5
murat-sever <murat-sever@live.com>
2023-11-26 12:34:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    let skills = ["HTML", "CSS", "JS", "React", "Node"];
+    let member = {
+        name: "John",
+        age: 23,
+        skills: skills,
+    };
+    // Write code here
+    member.skills.push("MongoDB");
+    member.skills.push("Express");
+    member.skills.push("React");
+    member.skills.push("Node");
+    // Don’t edit the code below here
+    return member;
+}
